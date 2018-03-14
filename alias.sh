c155da90c08ab52e8970115b1597569d6210f130
Wu Ming-Hsueh <ri3567@gmail.com>
2018-03-14 11:32:36 +0800

    fix nginx default set and add alias.sh

--- alias.sh
+++ alias.sh
@@ -1,6 +1,28 @@
-echo "alias php='docker exec -it php php'" >> ~/.bashrc
-echo "alias composer='docker exec -it -u $(id -nu) php composer'" >> ~/.bashrc
-echo "alias phpunit='docker exec -it -u $(id -nu)  php phpunit'" >> ~/.bashrc
-echo "alias laravel='docker exec -it -u $(id -nu) php /home/$(id -nu)/.composer/vendor/bin/laravel'" >> ~/.bashrc
-echo "alias php-cs-fixer='docker exec -it -u $(id -nu) /var/$(id -nu)/.composer/vendor/bin/php-cs-fixer'" >> ~/.bashrc
+#!/bin/bash
+echo -e 'alias php="docker exec -it php php"' >> ~/.bashrc
+echo -e 'alias phpunit="docker exec -it -u $(id -nu)  php phpunit"' >> ~/.bashrc
+echo -e 'function composer() {
+        if [[ $PWD =~ "public_root" ]];
+        then
+           docker exec -it -u $(id -nu) -w /var/www/public_root${PWD#/*public_root} php composer -d=./ $@
+        else
+           docker exec -it -u $(id -nu) php composer -d=./ $@
+        fi
+}' >> ~/.bashrc
+echo -e 'function laravel() {
+    if [[ $PWD =~ "public_root" ]];
+    then
+        docker exec -it -u $(id -nu) -w /var/www/public_root${PWD#/*public_root} php /home/$(id -nu)/.composer/vendor/bin/laravel $@
+    else
+        docker exec -it -u $(id -nu) php /home/$(id -nu)/.composer/vendor/bin/laravel $@
+    fi
+}' >> ~/.bashrc
+echo -e 'function php-cs-fixer() {
+    if [[ $PWD =~ "public_root" ]];
+    then
+        docker exec -it -u $(id -nu) -w /var/www/public_root${PWD#/*public_root} php /home/$(id -nu)/.composer/vendor/bin/php-cs-fixer $@
+    else
+        docker exec -it -u $(id -nu) php /home/$(id -nu)/.composer/vendor/bin/php-cs-fixer $@
+    fi
+}' >> ~/.bashrc
 source ~/.bashrc
